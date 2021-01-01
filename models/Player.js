8d53244f2de1573c20499105df64205bb358afa3
EmreKeskin47 <43786018+EmreKeskin47@users.noreply.github.com>
2021-01-01 20:19:23 +0300

    created Player model

---
+++ models/Player.js
@@ -0,0 +1,24 @@
+class Player {
+    constructor(playerName, playerPosition, overall) {
+        this.playerName = playerName;
+        this.playerPosition = playerPosition;
+        this.overall = overall;
+    }
+    //Attribute Details
+    /*
+    pace: 
+    shooting: 
+    passing: 
+    dribbling: 
+    defending: 
+    physical: 
+    //Statics
+    matchesPlayed: 
+    minutesPlayed: 
+    noOfGoals: 
+    noOfAssists: 
+    noOfRed: 
+    noOfYellow:
+    */
+}
+export default Player;
