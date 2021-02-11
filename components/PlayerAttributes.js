b8e3f29c13980363418bbc37c562ee0bfd0bc1fe
SimonYuvarlak <simonyuvarlak@icloud.com>
2021-02-11 12:18:17 +0300

    before progressbar

--- components/PlayerAttributes.js
+++ components/PlayerAttributes.js
@@ -5,98 +5,99 @@ import PlayerForm from "../components/PlayerForm";
 import * as playerActions from "../store/players-action";
 
 const PlayerAttributes = () => {
+  return (
+    <SafeAreaView style={styles.root}>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Pas</Text>
+          <Text style={styles.itemR}>67</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Sut</Text>
+          <Text style={styles.itemR}>81</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Fiz</Text>
+          <Text style={styles.itemR}>86</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Hiz</Text>
+          <Text style={styles.itemR}>73</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Def</Text>
+          <Text style={styles.itemR}>41</Text>
+        </View>
+      </View>
 
-    return (    
-        <SafeAreaView style={styles.root}>
-            
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Pas</Text>
-                        <Text style={styles.itemR}>67</Text>
-                    </View>
-                </View>
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Sut</Text>
-                        <Text style={styles.itemR}>81</Text>
-                    </View>
-                </View>
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Fiz</Text>
-                        <Text style={styles.itemR}>86</Text>
-                    </View>
-                </View>
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Hiz</Text>
-                        <Text style={styles.itemR}>73</Text>
-                    </View>
-                </View>
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Def</Text>
-                        <Text style={styles.itemR}>41</Text>
-                    </View>
-                </View>
-
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Yas</Text>
-                        <Text style={styles.itemR}>16</Text>
-                    </View>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Ayak</Text>
-                        <Text style={styles.itemR}>S</Text>
-                    </View>
-                </View>
-                <View style={styles.row}>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Boy</Text>
-                        <Text style={styles.itemR}>1.79</Text>
-                    </View>
-                    <View style={styles.column}>
-                        <Text style={styles.itemL}>Uyruk</Text>
-                        <Text style={styles.itemR}>tr</Text>
-                    </View>
-                </View>
-            
-          </SafeAreaView>
-    );
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Yas</Text>
+          <Text style={styles.itemR}>16</Text>
+        </View>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Ayak</Text>
+          <Text style={styles.itemR}>S</Text>
+        </View>
+      </View>
+      <View style={styles.row}>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Boy</Text>
+          <Text style={styles.itemR}>1.79</Text>
+        </View>
+        <View style={styles.column}>
+          <Text style={styles.itemL}>Uyruk</Text>
+          <Text style={styles.itemR}>tr</Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    root : {
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'space-around',   
-    },
-    row: {
-        justifyContent: 'center',
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    column: {
-        height: 30,
-        borderRadius: 8,
-        backgroundColor: 'black',
-        opacity: 0.5,
-        flex: 1,
-        margin: 20,
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        flexDirection: 'row'
-    },
-    itemL: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white'
-    },
-    itemR: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'red'
-    }
+  root: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-around",
+    backgroundColor: "black",
+    opacity: 0.5,
+    borderRadius: 8,
+  },
+  bar: {
+    backgroundColor: "white",
+  },
+  row: {
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  column: {
+    height: 30,
+    borderRadius: 8,
+    flex: 1,
+    margin: 20,
+    justifyContent: "space-around",
+    alignItems: "center",
+    flexDirection: "row",
+  },
+  itemL: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "white",
+  },
+  itemR: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "red",
+  },
 });
 
 export default PlayerAttributes;
--- screens/PlayerProfile.js
+++ screens/PlayerProfile.js
@@ -6,7 +6,9 @@ import {
   SafeAreaView,
   ImageBackground,
   Image,
+  ScrollView,
 } from "react-native";
+import { ProgressBar, Colors } from "react-native-paper";
 import { useDispatch, useSelector } from "react-redux";
 import PlayerAttributes from "../components/PlayerAttributes";
 import PlayerForm from "../components/PlayerForm";
@@ -18,45 +20,47 @@ const PlayerProfilePage = () => {
 
   return (
     <View style={styles.root}>
-      <ImageBackground source={image} style={styles.image}>
-        <View style={styles.topContainer}>
-          <Image source={profilePicture} style={styles.profilePicture} />
+      <ScrollView>
+        <ImageBackground source={image} style={styles.image}>
+          <View style={styles.topContainer}>
+            <Image source={profilePicture} style={styles.profilePicture} />
+            <View style={styles.column}>
+              <View style={styles.columnInside}>
+                <Text style={styles.itemL}>Isim</Text>
+                <Text style={styles.itemR}>Ronaldo</Text>
+              </View>
+              <View style={styles.columnInside}>
+                <Text style={styles.itemL}>Takim</Text>
+                <Text style={styles.itemR}>Galatasaray</Text>
+              </View>
+              <View style={styles.columnInside}>
+                <Text style={styles.itemL}>Mevki</Text>
+                <Text style={styles.itemR}>Forvet</Text>
+              </View>
+              <View style={styles.columnInside}>
+                <Text style={styles.itemL}>Numara</Text>
+                <Text style={styles.itemR}>9</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.surround}>
+            <PlayerAttributes />
+          </View>
           <View style={styles.column}>
-            <View style={styles.columnInside}>
-              <Text style={styles.itemL}>Isim</Text>
-              <Text style={styles.itemR}>Ronaldo</Text>
+            <View style={{ justifyContent: "center", flexDirection: "row" }}>
+              <Text style={styles.itemM}>Istatistik</Text>
             </View>
             <View style={styles.columnInside}>
-              <Text style={styles.itemL}>Takim</Text>
-              <Text style={styles.itemR}>Galatasaray</Text>
+              <Text style={styles.itemL}>Gol</Text>
+              <Text style={styles.itemR}>23</Text>
             </View>
             <View style={styles.columnInside}>
-              <Text style={styles.itemL}>Mevki</Text>
-              <Text style={styles.itemR}>Forvet</Text>
-            </View>
-            <View style={styles.columnInside}>
-              <Text style={styles.itemL}>Numara</Text>
-              <Text style={styles.itemR}>9</Text>
+              <Text style={styles.itemL}>Asist</Text>
+              <Text style={styles.itemR}>6</Text>
             </View>
           </View>
-        </View>
-        <View style={styles.surround}>
-          <PlayerAttributes />
-        </View>
-        <View style={styles.column}>
-          <View style={{ justifyContent: "center", flexDirection: "row" }}>
-            <Text style={styles.itemM}>Istatistik</Text>
-          </View>
-          <View style={styles.columnInside}>
-            <Text style={styles.itemL}>Gol</Text>
-            <Text style={styles.itemR}>23</Text>
-          </View>
-          <View style={styles.columnInside}>
-            <Text style={styles.itemL}>Asist</Text>
-            <Text style={styles.itemR}>6</Text>
-          </View>
-        </View>
-      </ImageBackground>
+        </ImageBackground>
+      </ScrollView>
     </View>
   );
 };
@@ -69,6 +73,7 @@ const styles = StyleSheet.create({
   surround: {
     height: 400,
     marginTop: 40,
+    marginHorizontal: 20,
   },
   image: {
     flex: 1,
