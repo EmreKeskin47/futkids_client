6ab8388f11c57bb516fcc530f6da261b3a082fee
SimonYuvarlak <simonyuvarlak@icloud.com>
2021-03-18 08:05:39 +0300

    add secondary color download linear gradient for vote page and player card component

--- components/PlayerCard.js
+++ components/PlayerCard.js
@@ -6,70 +6,68 @@ import PlayerSummary from "../components/PlayerSummary";
 import { Ionicons } from "@expo/vector-icons";
 import { RFPercentage } from "react-native-responsive-fontsize";
 import { ActivityIndicator } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 const PlayerCard = (props) => {
-    const { playerID, name, position, overall } = props.cardData;
-    return (
-        <View style={{ padding: 20 }}>
-            <Card style={styles.cardBackground}>
-                <CardItem header bordered style={styles.cardHeader}>
-                    <View style={styles.imageContainer}>
-                        <View>
-                            <Ionicons
-                                name="person-circle-sharp"
-                                style={styles.playerImage}
-                            />
-                        </View>
-                        <View>
-                            <Text style={styles.subheading}>{position}</Text>
-                            <Text style={styles.subheading}>{overall}</Text>
-                            <Text style={styles.playerName}>{name}</Text>
-                        </View>
-                    </View>
-                    <CardItem style={{ backgroundColor: Colors.primary }}>
-                        {props.attr ? (
-                            <PlayerSummary id={playerID} attr={props.attr} />
-                        ) : (
-                            <ActivityIndicator size="large" />
-                        )}
-                    </CardItem>
-                </CardItem>
-            </Card>
-        </View>
-    );
+  const { playerID, name, position, overall } = props.cardData;
+  return (
+    <View style={{ padding: 20 }}>
+      <Card style={styles.cardBackground}>
+        <CardItem header bordered style={styles.cardHeader}>
+          <View style={styles.imageContainer}>
+            <View>
+              <Ionicons name="person-circle-sharp" style={styles.playerImage} />
+            </View>
+            <View>
+              <Text style={styles.subheading}>{position}</Text>
+              <Text style={styles.subheading}>{overall}</Text>
+              <Text style={styles.playerName}>{name}</Text>
+            </View>
+          </View>
+          <CardItem style={{ backgroundColor: Colors.primary }}>
+            {props.attr ? (
+              <PlayerSummary id={playerID} attr={props.attr} />
+            ) : (
+              <ActivityIndicator size="large" />
+            )}
+          </CardItem>
+        </CardItem>
+      </Card>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
-    cardBackground: {
-        backgroundColor: Colors.primary,
-        borderRadius: 20,
-    },
-    imageContainer: {
-        width: "100%",
-        flexDirection: "row",
-        justifyContent: "space-between",
-    },
-    subheading: {
-        fontSize: RFPercentage(2.5),
-        color: "#fff",
-        textAlign: "right",
-    },
-    playerName: {
-        fontSize: RFPercentage(3),
-        fontWeight: "900",
-        color: "#fff",
-        textAlign: "center",
-    },
-    cardHeader: {
-        backgroundColor: Colors.primary,
-        flexDirection: "column",
-        marginHorizontal: 20,
-        borderRadius: 8,
-        marginTop: 10,
-    },
-    playerImage: {
-        fontSize: 80,
-    },
+  cardBackground: {
+    backgroundColor: Colors.primary,
+    borderRadius: 20,
+  },
+  imageContainer: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  subheading: {
+    fontSize: RFPercentage(2.5),
+    color: "#fff",
+    textAlign: "right",
+  },
+  playerName: {
+    fontSize: RFPercentage(3),
+    fontWeight: "900",
+    color: "#fff",
+    textAlign: "center",
+  },
+  cardHeader: {
+    backgroundColor: Colors.primary,
+    flexDirection: "column",
+    marginHorizontal: 20,
+    borderRadius: 8,
+    marginTop: 10,
+  },
+  playerImage: {
+    fontSize: 80,
+  },
 });
 
 export default PlayerCard;
