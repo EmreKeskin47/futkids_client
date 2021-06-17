616f4f56bd89a0d6413fa18b83414d11b31ec31b
SimonYuvarlak <simonyuvarlak@icloud.com>
2021-06-18 02:06:09 +0300

    kullanici changed to oyuncu listesi

--- screens/admin/post/AdminPostList.js
+++ screens/admin/post/AdminPostList.js
@@ -8,70 +8,64 @@ import PostList from "../../../components/PostList";
 import * as postActions from "../../../redux/actions/post-actions";
 
 const AdminPostList = (props) => {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(postActions.fetchPosts());
+  useEffect(() => {
+    dispatch(postActions.fetchPosts());
+  });
+  const postSelected = (item) => {
+    props.navigation.push("Post Details", {
+      id: item.id,
+      tit: item.title,
+      img: item.image,
+      description: item.description,
     });
-    const postSelected = (item) => {
-        props.navigation.push("Post Details", {
-            id: item.id,
-            tit: item.title,
-            img: item.image,
-            description: item.description,
-        });
-    };
+  };
 
-    return (
-        <TouchableOpacity>
-            <PostList onPress={postSelected} />
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity>
+      <PostList onPress={postSelected} />
+    </TouchableOpacity>
+  );
 };
 
 export const screenOptions = (navData) => {
-    return {
-        headerTitle: "Haber Listesi",
-        headerLeft: () => (
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item
-                    title="Menu"
-                    iconName={
-                        Platform.OS === "android" ? "md-menu" : "ios-menu"
-                    }
-                    onPress={() => {
-                        navData.navigation.toggleDrawer();
-                    }}
-                />
-            </HeaderButtons>
-        ),
-        headerRight: () => (
-            <HeaderButtons HeaderButtonComponent={HeaderButton}>
-                <Item
-                    title="New"
-                    iconName={
-                        Platform.OS === "android"
-                            ? "add-circle"
-                            : "ios-add-circle"
-                    }
-                    onPress={() => {
-                        navData.navigation.push("Create Post");
-                    }}
-                />
-            </HeaderButtons>
-        ),
-    };
+  return {
+    headerTitle: "Haber Listesi",
+    headerLeft: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="Menu"
+          iconName={Platform.OS === "android" ? "md-menu" : "ios-menu"}
+          onPress={() => {
+            navData.navigation.toggleDrawer();
+          }}
+        />
+      </HeaderButtons>
+    ),
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item
+          title="New"
+          iconName={Platform.OS === "android" ? "add-circle" : "ios-add-circle"}
+          onPress={() => {
+            navData.navigation.push("Create Post");
+          }}
+        />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100%",
-    },
-    text: {
-        paddingVertical: 25,
-    },
+  root: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+  },
+  text: {
+    paddingVertical: 25,
+  },
 });
 export default AdminPostList;
